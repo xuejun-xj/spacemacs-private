4483c1f0a9a6db0810fb69f85454f2e53b806a6c
xuejun-xj <xuejun.0128@gmail.com>
2022-08-07 16:03:36 +0800

    Day 14: file and buffer
    Learn file and buffer operations.
    Add useful keybindings.
    
    Signed-off-by: xuejun-xj <xuejun.0128@gmail.com>

--- layers/xuejun-learn/keybindings.el
+++ layers/xuejun-learn/keybindings.el
@@ -15,3 +15,22 @@
   (call-interactively 'occur))
 
 (global-set-key (kbd "M-s o") 'occur-dwim)
+
+;; useful file bindings
+(spacemacs/set-leader-keys "ff" 'helm-find-files)
+(spacemacs/set-leader-keys "fL" 'helm-locate)
+(spacemacs/set-leader-keys "fel" 'helm-locate-library)
+
+;; project find files
+(defun xuejun-learn/open-file-with-projectile-or-counsel-git ()
+  (interactive)
+  (counsel-git))
+;;  (if (xuejun-learn/vcsproject-root)
+;;      (counsel-git)
+;;    (if (projectile-project-p)
+;;        (projectile-find-file)
+;;      (ido-find-file))))
+(spacemacs/set-leader-keys "pf" 'xuejun-learn/open-file-with-projectile-or-counsel-git)
+
+;; useful buffer bindings
+(spacemacs/set-leader-keys "bi" 'ibuffer)
